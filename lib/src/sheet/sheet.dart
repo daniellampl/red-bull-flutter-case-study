99386a0795ba9ddb0dac176c6da1ac4a4b3f2ee6
daniellampl <dnl.lampl@gmail.com>
2024-01-24 15:23:42 -0500

    feat: add code of sheet package to project for Cupertino style sheet route animations

---
+++ lib/src/sheet/sheet.dart
@@ -0,0 +1,12 @@
+export 'src/physics.dart';
+export 'src/scroll_controller.dart';
+export 'src/scrollable.dart';
+export 'src/sheet.dart';
+export 'src/widgets/default_sheet_controller.dart';
+export 'src/widgets/sheet_media_query.dart';
+
+export 'src/route/base_route_extended.dart';
+export 'src/route/cupertino/sheet_route.dart'
+    hide CupertinoSheetBottomRouteTransition;
+export 'src/route/delegated_transitions_route.dart';
+export 'src/route/sheet_route.dart';
